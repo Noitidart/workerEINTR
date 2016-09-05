de96af64b830aa155eb98dabc94475e0bacc0221
Noitidart <noitidart@gmail.com>
2016-09-05 10:14:56 -0700

    Update worker.js
    this shows that if there is stuff written to pipe, read will not EINTR, however i cant get read to block on empty pipe, which is what i am trying to accomplish

--- worker.js
+++ worker.js
@@ -3,6 +3,11 @@ var read = lib.declare('read', ctypes.default_abi, ctypes.ssize_t, ctypes.int, c
 var pipe = lib.declare('pipe', ctypes.default_abi, ctypes.int, ctypes.int.array(2));
 var write = lib.declare('write', ctypes.default_abi, ctypes.ssize_t, ctypes.int, ctypes.void_t.ptr, ctypes.size_t);
 
+// var fread = lib.declare('fread', ctypes.default_abi, ctypes.size_t, ctypes.void_t.ptr, ctypes.size_t, ctypes.size_t, ctypes.voidptr_t);
+// var fwrite = lib.declare('fread', ctypes.default_abi, ctypes.size_t, ctypes.void_t.ptr, ctypes.size_t, ctypes.size_t, ctypes.voidptr_t);
+
+
+
 var pfd = ctypes.int.array(2)();
 var rez_pipe = pipe(pfd);
 console.log('rez_pipe:', rez_pipe);
@@ -14,18 +19,29 @@ var pipe_r = pfd[0];
 var pipe_w = pfd[1];
 console.log('pipe_r:', pipe_r, 'pipe_w:', pipe_w);
 
-var i=0;
-while (i++<100) {
-	var buf = ctypes.char.array(10)();
-	var rez_read = read(pipe_r, buf, buf.constructor.size);
-	if (rez_read.toString() === '-1') {
-		if (ctypes.errno === 4) {
-		    console.warn('got EINTR');
-		} else {
-			throw new Error('failed to `read`, for non-EINTR errno: ' + ctypes.errno);
-		}
-	} else {
-		var len = rez_read;
-		console.log('succesfully read, len:', len);
-	}
+
+var wbuf = ctypes.char.array(2)('ya');
+var rez_write = write(pipe_w, wbuf, wbuf.constructor.size);
+console.log('rez_write:', rez_write);
+if (rez_write.toString() === '-1') {
+    console.error('failed to write!!!!! wtf? errno:', ctypes.errno);
+} else {
+
+    var i=0;
+    while (i++<1000) {
+        var buf = ctypes.char.array(10)();
+        var rez_read = read(pipe_r, buf, buf.constructor.size);
+        if (rez_read.() === '-1') {
+            if (ctypes.errno === 4) {
+                console.warn('got EINTR');
+            } else {
+                throw new Error('failed to `read`, for non-EINTR errno: ' + ctypes.errno);
+            }
+        } else {
+            var len = rez_read;
+            console.log('succesfully read, len:', len);
+            break;
+        }
+    }
+
 }
